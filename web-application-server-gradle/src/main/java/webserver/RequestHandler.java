069a847560d3d8c28fde9f8a2ad3263821ebfb75
LEESEUNGYEOL <lsy12210@gmail.com>
2024-03-11 19:28:05 +0900

    feat: [#5] 회원가입 시 회원 정보 DataBase에 저장

--- web-application-server-gradle/src/main/java/webserver/RequestHandler.java
+++ web-application-server-gradle/src/main/java/webserver/RequestHandler.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +24,7 @@ public class RequestHandler extends Thread {
 
     @Override
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
@@ -40,25 +40,29 @@ public class RequestHandler extends Thread {
                 log.debug("header: {}", line);
                 line = br.readLine();
                 String[] headerTokens = line.split(": ");
-                if(headerTokens.length == 2) {
+                if (headerTokens.length == 2) {
                     headers.put(headerTokens[0], headerTokens[1]);
                 }
             }
 
+            //URL 분기 처리
             if (url.startsWith("/user/create")) {
                 int contentLength = Integer.parseInt(headers.get("Content-Length"));
                 String requestBody = IOUtils.readData(br, contentLength);
                 log.debug("Request Body: {}", requestBody);
                 Map<String, String> paramMap = HttpRequestUtils.parseQueryString(requestBody);
-                User user = new User(paramMap.get("userId"), paramMap.get("password"),
-                        paramMap.get("name"), paramMap.get("email"));
-                log.debug("User : {}", user);
+                User user = new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email"));
 
-                url= "/index.html";
+                //User 저장
+                DataBase.addUser(user);
+                log.debug("User : {}", user);
+                log.debug("DataBase : {}", DataBase.findAll());
+
+                url = "/index.html";
                 DataOutputStream dos = new DataOutputStream(out);
                 response302Header(dos, url);
 
-            }else{
+            } else {
                 DataOutputStream dos = new DataOutputStream(out);
                 byte[] body = Files.readAllBytes(new File("./web-application-server-gradle/webapp" + url).toPath());
                 response200Header(dos, body.length);
