b13daefe7fee0af46c5eb3384f0aac654c200f37
LEESEUNGYEOL <lsy12210@gmail.com>
2024-03-25 19:33:13 +0900

    feat: [#8] Http Method 추가

---
+++ web-application-server-gradle/src/main/java/webserver/http/HttpMethod.java
@@ -0,0 +1,14 @@
+package webserver.http;
+
+public enum HttpMethod {
+    GET, POST;
+
+    public boolean isGet() {
+        return this == GET;
+    }
+
+    public boolean isPost() {
+        return this == POST;
+    }
+
+}
